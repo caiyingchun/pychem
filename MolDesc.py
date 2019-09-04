5ef34095417c4547eef9fc717247d84a37695c44
Yingchun Cai <38513789+caiyingchun@users.noreply.github.com>
2019-09-04 08:13:04 +0800

    Update MolDesc.py

--- MolDesc.py
+++ MolDesc.py
@@ -4,9 +4,9 @@ from rdkit.Chem import DataStructs
 from rdkit.Chem import Descriptors
 from rdkit.ML.Descriptors import MoleculeDescriptors
 
-trainset = [mol for mol in Chem.SDMolSupplier('mols.sdf') if mol is not None]
+Molset = [mol for mol in Chem.SDMolSupplier('mols.sdf') if mol is not None]
 nms=[x[0] for x in Descriptors._descList]
 calc = MoleculeDescriptors.MolecularDescriptorCalculator(nms)
-trainDescrs = [calc.CalcDescriptors(x) for x in trainset]
-trainDescrs = np.array(trainDescrs)
-print trainDescrs
+MolDescrs = [calc.CalcDescriptors(x) for x in Molset]
+MolDescrs = np.array(MolDescrs)
+print MolDescrs
