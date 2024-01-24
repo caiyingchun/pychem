19ad1378c29209cf3839e2444a7d46d53d86b9d2
cai_yc <38513789+caiyingchun@users.noreply.github.com>
2024-01-24 15:27:49 +0800

    Update split_sdf.py

--- split_sdf.py
+++ split_sdf.py
@@ -1,15 +1,24 @@
-f = 'your_file_name'
-split_number= 1000 #number of molecules in each file
+fname = 'CNS_compound'
+split_number= 2 #number of molecules in each file
 number_of_sdfs = split_number
-i = 0; j = 0
-f2 = open(f + '_' + str(j) + '.sdf', 'w')
-for line in open(f + '.sdf'):
-    f2.write(line)
-    if line[:4] == "$$$$":
-        i += 1
-    if i > number_of_sdfs:
-        number_of_sdfs += split_number 
-        f2.close()
-        j += 1
-        f2 = open(f + '_' + str(j) + '.sdf','w')
-print(i)
+i = 0; j = 0; k = 0
+with open(f'{fname}.sdf') as f:
+    for line in f:
+        if "$$$$" in line:
+            k += 1
+print(f'There are {k} molecules in {fname}.sdf')
+
+f2 = open(f'{fname}_{j}.sdf', 'w')
+with open(f'{fname}.sdf') as f:
+    for line in f:
+        f2.write(line)
+        if "$$$$" in line:
+            i += 1
+        if i == k:
+            f2.close()
+            break
+        if i == number_of_sdfs:
+            number_of_sdfs += split_number 
+            f2.close()
+            j += 1
+            f2 = open(f'{fname}_{j}.sdf', 'w')
